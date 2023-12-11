beace2bdd721fbd08c6ed0e3e36ea92981b0e587
LorryGailius <lauris.gailius@gmail.com>
2023-12-11 11:10:06 +0200

    Added /customer/coupon endpoints

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -2,12 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using PositronAPI.Models;
 using System.ComponentModel.DataAnnotations;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace PositronAPI.Controllers
 {
     public class CustomersController : ControllerBase
     {
+        // Customer Endpoints
+
         /// <summary>
         /// Add a customer
         /// </summary>
@@ -70,7 +71,7 @@ namespace PositronAPI.Controllers
             return StatusCode(StatusCodes.Status501NotImplemented);
         }
 
-        /// TODO: Add methods for Coupons and LoyaltyCards 
+        // Loyalty Card Endpoints
 
         /// <summary>
         /// Add a loyalty card for customer
@@ -109,6 +110,58 @@ namespace PositronAPI.Controllers
             return StatusCode(StatusCodes.Status501NotImplemented);
         }
 
+        // Coupon Endpoints
+
+        /// <summary>
+        /// Add a coupon for customer
+        /// </summary>
+        /// <remarks>Creates a coupon.</remarks>
+        /// <param name="body">Properties for creating a new coupon.</param>
+        /// <param name="customerId">The id of the coupon holder</param>
+        [HttpPost]
+        [Route("/customer/{customerId}/coupon")]
+        public virtual IActionResult CreateCoupon([FromBody] CreateCoupon body, [FromRoute][Required] string customerId)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        /// <summary>
+        /// Remove a coupon
+        /// </summary>
+        /// <remarks>Deletes a coupon.</remarks>
+        /// <param name="customerId">The id of the coupon holder</param>
+        /// <param name="couponId">The id of the coupon</param>
+        [HttpDelete]
+        [Route("/customer/{customerId}/coupon/{couponId}")]
+        public virtual IActionResult DeleteCoupon([FromRoute][Required] string customerId, [FromRoute][Required] string couponId)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        /// <summary>
+        /// Get a coupon of a customer
+        /// </summary>
+        /// <remarks>Gets a coupon.</remarks>
+        /// <param name="customerId">The id of the coupon holder</param>
+        /// <param name="couponId">The id of the coupon</param>
+        [HttpGet]
+        [Route("/customer/{customerId}/coupon/{couponId}")]
+        public virtual IActionResult GetCoupon([FromRoute][Required] string customerId, [FromRoute][Required] string couponId)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        /// <summary>
+        /// Gets coupons of a customer
+        /// </summary>
+        /// <remarks>Gets couponss.</remarks>
+        /// <param name="customerId">The id of the coupon holder</param>
+        [HttpGet]
+        [Route("/customer/{customerId}/coupon")]
+        public virtual IActionResult GetCoupons([FromRoute][Required] string customerId)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
 
     }
 }
