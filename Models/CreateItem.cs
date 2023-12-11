4f6a813c69ff9c974f7c4f2d494f040386910bc1
LorryGailius <lauris.gailius@gmail.com>
2023-12-11 11:27:07 +0200

    Added Item classes

---
+++ Models/CreateItem.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace PositronAPI.Models
+{
+    public class CreateItem : IEquatable<CreateItem>
+    {
+        /// <summary>
+        /// Gets or Sets Name
+        /// </summary>
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Description
+        /// </summary>
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Price
+        /// </summary>
+
+        [DataMember(Name = "price")]
+        public decimal? Price { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Stock
+        /// </summary>
+
+        [DataMember(Name = "stock")]
+        public int? Stock { get; set; }
+
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class CreateItem {\n");
+            sb.Append("  Name: ").Append(Name).Append("\n");
+            sb.Append("  Description: ").Append(Description).Append("\n");
+            sb.Append("  Price: ").Append(Price).Append("\n");
+            sb.Append("  Stock: ").Append(Stock).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((CreateItem)obj);
+        }
+
+        /// <summary>
+        /// Returns true if CreateItem instances are equal
+        /// </summary>
+        /// <param name="other">Instance of CreateItem to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(CreateItem other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return
+                (
+                    Name == other.Name ||
+                    Name != null &&
+                    Name.Equals(other.Name)
+                ) &&
+                (
+                    Description == other.Description ||
+                    Description != null &&
+                    Description.Equals(other.Description)
+                ) &&
+                (
+                    Price == other.Price ||
+                    Price != null &&
+                    Price.Equals(other.Price)
+                ) &&
+                (
+                    Stock == other.Stock ||
+                    Stock != null &&
+                    Stock.Equals(other.Stock)
+                );
+        }
+    }
+}
---
+++ Models/ItemCategories.cs
@@ -0,0 +1,27 @@
+﻿namespace PositronAPI.Models
+{
+    public enum ItemCategories
+    {
+        // Barbershop Categories
+        Haircuts,
+        Shaving,
+        HairStyling,
+        HaircareProducts,
+        SkincareProducts,
+        BarberServices, // for other general services
+
+        // Restaurant Categories
+        Appetizers,
+        MainCourses,
+        Desserts,
+        Beverages,
+        AlcoholicDrinks,
+        NonAlcoholicDrinks,
+        SpecialtyDishes, // for signature or unique dishes
+
+        // Common Categories
+        Merchandise, // like branded items, t-shirts, etc.
+        Memberships, // for subscription or loyalty programs
+        SpecialOffers // for promotions or special deals
+    }
+}
