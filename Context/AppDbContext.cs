4d5120876715b2bf5a23dab4116ba05e0bc153f6
Laurynas <lauris.gailius@gmail.com>
2023-12-11 18:30:58 +0200

    Added PostgresSQL database

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PositronAPI.Models;
+
+namespace PositronAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        DbSet<Customer> customers { get; set; }
+
+        DbSet<Coupon> coupons { get; set; }
+
+        DbSet<Item> items { get; set; }
+
+        DbSet<LoyaltyCard> loyaltyCards { get; set; }
+
+    }
+}
