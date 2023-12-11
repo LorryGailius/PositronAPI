ce36c5da68fda27f301cb29bd78b5998c08e04c6
LorryGailius <lauris.gailius@gmail.com>
2023-12-11 10:38:28 +0200

    Added basic layout for Customer endpoint

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PositronAPI.Models;
+using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace PositronAPI.Controllers
+{
+    public class CustomersController : ControllerBase
+    {
+        /// <summary>
+        /// Add a customer
+        /// </summary>
+        /// <remarks>Creates a customer.</remarks>
+        /// <param name="body">Properties for creating a new customer.</param>
+        [HttpPost]
+        [Route("/customer")]
+        public virtual IActionResult CreateCustomer([FromBody] CreateCustomer body)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        /// <summary>
+        /// Remove a customer
+        /// </summary>
+        /// <remarks>Deletes a customer.</remarks>
+        /// <param name="customerId">The id of the customer</param>
+        [HttpDelete]
+        [Route("/customer/{customerId}")]
+        public virtual IActionResult DeleteCustomer([FromRoute][Required] string customerId)
+        {
+               return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        /// <summary>
+        /// Edit a customer
+        /// </summary>
+        /// <remarks>Edits a customer.</remarks>
+        /// <param name="body">Properties for creating a new customer.</param>
+        /// <param name="customerId">The id of the customer</param>
+        [HttpPut]
+        [Route("/customer/{customerId}")]
+        public virtual IActionResult EditCustomer([FromBody] CreateCustomer body, [FromRoute][Required] string customerId)
+        {
+                  return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        /// <summary>
+        /// Get a customer
+        /// </summary>
+        /// <remarks>Gets a customer.</remarks>
+        /// <param name="customerId">The id of the customer to get</param>
+        [HttpGet]
+        [Route("/customer/{customerId}")]
+        public virtual IActionResult GetCustomer([FromRoute][Required] string customerId)
+        {
+               return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        /// <summary>
+        /// Gets all customers
+        /// </summary>
+        /// <remarks>Get all items.</remarks>
+        /// <param name="top">The number of customers to get</param>
+        /// <param name="skip">The number of customers to skip over</param>
+        [HttpGet]
+        [Route("/customer")]
+        public virtual IActionResult GetCustomers([FromQuery] decimal? top, [FromQuery] decimal? skip)
+        {
+               return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+
+        /// TODO: Add methods for Coupons and LoyaltyCards 
+
+    }
+}
