a39a8fc8f9b1747d8d575614e56c9bf5fb90a814
bamboozlegui <kartoffelgutgut@gmail.com>
2023-12-22 14:15:40 +0200

    create ICustomerService interface

---
+++ Interfaces/ICustomerService.cs
@@ -0,0 +1,13 @@
+using PositronAPI.Models.Customer;
+
+namespace PositronAPI.Services.CustomerService
+{
+    public interface ICustomerService
+    {
+        Task<Customer> CreateCustomer(Customer customer);
+        Task<Customer> DeleteCustomer(long customerId);
+        Task<Customer> EditCustomer(Customer customer, long customerId);
+        Task<Customer> GetCustomer(long customerId);
+        Task<List<Customer>> GetCustomers(int top = 10, int skip = 0);
+    }
+}
