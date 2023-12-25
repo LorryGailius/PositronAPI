b7bd6969c50360527c2f0fa74054f7f68f386cb9
bamboozlegui <kartoffelgutgut@gmail.com>
2023-12-25 21:09:51 +0200

    add ILoyaltyService interface

---
+++ Interfaces/ILoyaltyService.cs
@@ -0,0 +1,11 @@
+using PositronAPI.Models.LoyaltyCard;
+
+namespace PositronAPI.Services.LoyaltyService
+{
+    public interface ILoyaltyService
+    {
+        Task<LoyaltyCard> CreateLoyaltyCard(LoyaltyCard loyaltyCard);
+        Task<LoyaltyCard> DeleteLoyaltyCard(long customerId);
+        Task<LoyaltyCard> GetLoyaltyCard(long loyaltyCardId);
+    }
+}
