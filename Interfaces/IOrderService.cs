33016ed5646a908e562995eabb057010b239ecd8
bamboozlegui <kartoffelgutgut@gmail.com>
2024-01-10 21:25:52 +0200

    add IOrderService.cs interface

---
+++ Interfaces/IOrderService.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PositronAPI.Models.Order;
+
+namespace PositronAPI.Services.OrderService
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrder(Order order);
+        Task<ItemOrder> AddItemToOrder(ItemOrder itemOrder);
+        Task<ServiceOrder> AddServiceToOrder(ServiceOrder serviceOrder);
+        Task<Order> GetOrder(long orderId);
+        Task<List<ItemOrder>> GetOrderItems(long orderId);
+        Task<List<ServiceOrder>> GetOrderServices(long orderId);
+        Task<Order> EditOrder(Order order, long orderId);
+        decimal Subtotal(decimal price, int quantity);
+    }
+}
